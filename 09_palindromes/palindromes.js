76ff918771d14e7ff4be9f5ef75d78fe77e38aa1
boy-dad <manlapaz.jay@gmail.com>
2021-12-13 11:40:41 +0800

    palindrome function prototype

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,8 @@
-const palindromes = function () {
-
+const palindromes = function (x) {
+    x = x.replace(/\W/g, '').toLowerCase();
+    xSplitReversed = x.split('').reverse();
+    xReversed = xSplitReversed.join('');
+    return x === xReversed;
 };
 
 // Do not edit below this line
